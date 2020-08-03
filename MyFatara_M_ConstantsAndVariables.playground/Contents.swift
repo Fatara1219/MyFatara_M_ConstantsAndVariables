d683aa8d38f8a7dd213b0f65b2b949e684befaf1
Fatara1219 <67969041+Fatara1219@users.noreply.github.com>
2020-08-03 11:28:51 -0500

    Add files via upload

---
+++ MyFatara_M_ConstantsAndVariables.playground/Contents.swift
@@ -0,0 +1,119 @@
+import UIKit
+
+var str = "Hello, playground"
+let whereIamBorn = "Chicago"
+let whereIlive = "Bronzeville"
+let MyAge = "17"
+let MyFavoriteMovie = "Harry Potter Series"
+let InchesInAFoot = 12
+let FirstPresidentOfUS = "George Washington"
+
+var CurrentLocation = "Chicago"
+
+//Working with Variables
+var score = 0
+score = 10
+score = 15
+score = score + 5
+score + 5
+score
+score + 3
+score
+score = score + 5
+score = score + 3
+score = score + 5
+score = score + 5 + 3
+
+//A Shortcut
+score += 2
+score += 5
+score += 30
+
+let word1 = "Compound"
+let word2 = "assignment"
+let word3 = "is"
+let word4 = "useful"
+let space = " "
+
+var statement = ""
+statement = word1 + word2 + word3 + word4
+
+//Confusing Changes
+//It is very unlikely because the plan os constantly inconsistent and some may have decided to not go at that point.
+//There would be less mistakes if the plan stayed the same.
+
+//Safer Code in a Varying World
+var friendBeverageChoice = "coffee"
+friendBeverageChoice = "tea"
+friendBeverageChoice = "water"
+friendBeverageChoice = "sparkling water"
+friendBeverageChoice = "plain water"
+friendBeverageChoice = "nothing 😴"
+
+//Safer Code in an Unexpected World
+// Scores for each target
+var scoreForGreen = 5
+var scoreForRed = 10
+var scoreForGold = 20
+
+// Player scores
+var scoreForGary = 0
+var scoreForRob = 0
+
+// Game events here
+scoreForGary += scoreForRed
+scoreForGary += scoreForGreen
+scoreForGary += scoreForGold
+
+scoreForRob += scoreForRed
+scoreForRed += scoreForGreen
+scoreForRob += scoreForGold
+
+scoreForGary += scoreForRed
+scoreForGary += scoreForGreen
+scoreForGary += scoreForGold
+
+scoreForRob += scoreForRed
+scoreForRob += scoreForGreen
+scoreForRob += scoreForGold
+
+scoreForRob
+scoreForGary
+//Rob's score for Gold was miscalulated for only 15 points instead of the full 20 points.
+
+//Making a Shopping List
+var groceries = 5
+groceries
+let eggs = "Eggs "
+let newLine = "\n"
+newLine
+let cheese = "Cheese += milk"
+newLine
+let bread = "Bread"
+newLine
+let rice = "Rice"
+newLine
+
+//Exercise: 501
+var OverallScore1 = 501
+var Score = 0
+//Round 1
+Score = score + 16
+Score = score + 5
+Score = score + 10
+Score = score + 3
+//Round 2
+score = score + 4
+score = score + 12
+score = score + 2
+Score = score + 7
+//Round 3
+score = score + 5
+score = score + 3
+score = score + 2
+score = score + 1
+
+let currentscore = 291
+let CurrentOverallScore = OverallScore1 - currentscore
+var ResetScore = 0
+//I could improve the score to use subtraction when the dart hits a number lower than zero. When the number hits higher than the 10-20 range, you can add those points instead.
